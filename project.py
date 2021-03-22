4cf3e1c953dc04fe8ce04d4c24532f69c439e920
Zachnjue <njuezachary@gmail.com>
2021-03-22 20:18:36 +0300

    added .name method to capture name of the apps rather than the paths

--- project.py
+++ project.py
@@ -1,6 +1,18 @@
 # Finding the path to the apps on MacOS
 from pathlib import Path
 import os
-p = Path('/Applications')
-x = list(p.glob('*'))
-with open('listofApps.txt', 'w'):
+
+pathofFile = Path('/Applications')
+generatedList = list(pathofFile.glob('*'))
+#emptyList = []
+for item in generatedList:
+    print(item.name)
+    # emptyList.append(item)
+# print(emptyList[1])
+# sortList
+
+
+'''with open('listofApps.txt', 'w') as fileobject:
+    fileobject.write(str(x))
+    fileobject.close()
+'''
