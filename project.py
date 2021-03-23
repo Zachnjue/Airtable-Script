6cc02c415de2d7327d07217a037acd20be795c0d
Zachnjue <njuezachary@gmail.com>
2021-03-23 20:34:44 +0300

    added comments into the code

--- project.py
+++ project.py
@@ -2,13 +2,14 @@
 from pathlib import Path
 import os
 
+# Path containing the apps on the Mac
 pathofFile = Path('/Applications')
 generatedList = list(pathofFile.glob('*.app'))
 
+# Create an empty list where the apps will be stored
 emptyList = []
 for item in generatedList:
     emptyList.append(item.name)
-print(emptyList)
 
 with open('listofApps.txt', 'w') as fileobject:
     fileobject.write(str(emptyList))
